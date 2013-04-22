442ff093a5b6a0d330d89b29a67a1784406f96db
Dave <badave@gmail.com>
2013-04-22 01:33:06 -0700

    Saving messages

--- controllers/sms_controller.js
+++ controllers/sms_controller.js
@@ -9,10 +9,30 @@ var config = require("../config");
 
 var smsController = module.exports = {};
 
+/**
+ * Example request
+ * 2013-04-22T08:29:27.208017+00:00 app[web.1]:   Body: 'Ping',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   ToZip: '',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   FromState: 'NV',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   ToCity: '',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   SmsSid: 'SM0a07a18ae477b79e755bd552cdf84531',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   ToState: 'FL',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   To: '+19414445652',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   ToCountry: 'US',
+2013-04-22T08:29:27.208017+00:00 app[web.1]:   FromCountry: 'US',
+2013-04-22T08:29:27.208328+00:00 app[web.1]:   SmsMessageSid: 'SM0a07a18ae477b79e755bd552cdf84531',
+2013-04-22T08:29:27.208328+00:00 app[web.1]:   ApiVersion: '2010-04-01',
+2013-04-22T08:29:27.208328+00:00 app[web.1]:   FromCity: 'RENO',
+2013-04-22T08:29:27.208328+00:00 app[web.1]:   SmsStatus: 'received',
+2013-04-22T08:29:27.208328+00:00 app[web.1]:   From: '+17752879549'
+ */
 smsController.post = function(req, res, next) {
 	console.log(req.body);
   var context = {
     title: config.title
   }
-  res.send("");
+
+  db.insert("smses", req.body, function(err, object) {
+  	res.send("");
+  })
 }
