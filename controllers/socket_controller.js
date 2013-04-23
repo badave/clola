b03c154d49343d450fc42d2082c151d39b3302bd
Dave <badave@gmail.com>
2013-04-23 01:40:28 -0700

    removing singleton and simplifying

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -12,44 +12,24 @@ var url = require('url');
 var evt = require("../models/evt");
 
 var socketController = module.exports = function(server){
-	if(socketController.caller != socketController.getInstance) {
-		throw new Error("Do not instantiate this singleton class");
-	}
-
 	var self = this;
-	this.io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server);
 	// assuming io is the Socket.IO server object
-	this.io.configure(function () { 
-	  self.io.set("transports", ["xhr-polling"]); 
-	  self.io.set("polling duration", 10); 
+	io.configure(function () { 
+	  io.set("transports", ["xhr-polling"]); 
+	  io.set("polling duration", 10); 
 	});
 
-	this.io.on("connection", function(socket) {
+	io.on("connection", function(socket) {
 		// console.log(socket);
 		socket.emit("weee", {'data': "hello"});
 	})
+
+	evt.on("new_message", function(msg) {
+		io.sockets.emit("sms", msg);
+	})
+
 };
-
-evt.on("new_message", function(msg) {
-	socketController.getInstance().io.sockets.emit("sms", msg);
-})
-
-socketController.sockets = function() {
-	return this.io.sockets;
-}
-
-
-socketController.instance = null;
-socketController.getInstance = function(server) {
-	if(this.instance === null) {
-		if(!server) {
-			throw new Error("Server is null, but is needed -- create with a server");
-		}
-		this.instance = new socketController(server);
-	}
-	return this.instance;
-}
-
 
 
 // var io = require('socket.io').listen(80);
