3ab683287180525c8fba755c2f2cbf0733316003
Dave <badave@gmail.com>
2013-08-01 01:23:17 -0700

    Messages CSS and replying somewhat works

--- backbone/modules/messages/views/message_view.js
+++ backbone/modules/messages/views/message_view.js
@@ -1,10 +1,24 @@
 MessageView = Backbone.Marionette.ItemView.extend({
 	template_path: "messages/templates/message",
 	className: "message-view",
+	events: {
+		"submit form": "sendReply"
+	},
 	onRender: function() {
 		var that = this;
 		setTimeout(function() {
 			that.el.scrollTop = that.el.scrollHeight;
 		}, 10);
+	},
+	sendReply: function(e) {
+		e.preventDefault();
+
+		App.socket.emit("reply", { "phone": this.model.get("phone"), "message": {
+			"text": this.$el.find("#reply-box").val(), 
+			"reply": true, 
+			"created": new Date().getTime()
+		}});
+
+		$(e.target).find("input").val("");
 	}
 });
