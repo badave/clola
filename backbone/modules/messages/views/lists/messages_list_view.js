8341984ba54c27e337e12150a76befd361d5d2fa
Arpan Nanavati <arpan.nanavati@couchsurfing.com>
2013-08-01 21:47:52 -0700

    have a new notification icon when there is a new message from the end user

--- backbone/modules/messages/views/lists/messages_list_view.js
+++ backbone/modules/messages/views/lists/messages_list_view.js
@@ -1,7 +1,20 @@
 MessageRowView = ListRowView.extend({
 	render: function() {
 		this.$el.html(this.elem).append('<i class="icon-chevron-sign-right fr"></i>');
+		
+	  if(this.onRender) this.onRender();
 		return this;
+	},
+	renderNewMessage: function(model) {
+	  if(this.elem == model.attributes.phone) {
+	    this.$el.html(this.elem).append('<i class="icon-circle fr"></i>');
+	  }
+	},
+	onRender: function() {
+	  var that = this;
+	  App.vent.on('change:message', function(model) {
+	    that.renderNewMessage(model);
+	  })
 	},
 	onSelect: function() {
 		App.vent.trigger("message:selected", this.elem);
