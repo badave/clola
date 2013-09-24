af7fd3cac46a12a0e25ef93d44542777e5212ed2
Dave <badave@gmail.com>
2013-09-24 00:23:02 -0700

    Restyled the app page

--- controllers/rabbit_controller.js
+++ controllers/rabbit_controller.js
@@ -3,13 +3,26 @@ var amqp = require('amqp');
 var evt = require("../models/evt");
 
 var rabbitControllerClass = function() {
-  var rabbitConnection = amqp.createConnection({});
+  // { host: 'localhost'
+  // , port: 5672
+  // , login: 'guest'
+  // , password: 'guest'
+  // , authMechanism: 'AMQPLAIN'
+  // , vhost: '/'
+  // , ssl: { enabled : false
+  //        }
+  // }
+  var rabbitConnection = amqp.createConnection({ reconnect: true, keepAlive: 10000 });
 
-  rabbitConnection.setMaxListeners(100);
 
   var smsExchange;
-  rabbitConnection.on('ready', function () {
+  rabbitConnection.on('connect', function () {
+    console.log("Connected to rabbitmq");
     smsExchange = rabbitConnection.exchange('smsExchange', { 'type' : 'fanout'});
+  });
+
+  rabbitConnection.on("close", function() {
+    console.log("Connection with rabbitmq is closed");
   });
 
   this.smsExchange = smsExchange;
