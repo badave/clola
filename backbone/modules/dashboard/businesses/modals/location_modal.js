2aeb7c6b985d5b53e6a7bf22e97f4b6e3bdfe885
Dave <badave@gmail.com>
2013-10-08 00:31:51 -0700

    more bug fixes

--- backbone/modules/dashboard/businesses/modals/location_modal.js
+++ backbone/modules/dashboard/businesses/modals/location_modal.js
@@ -1,6 +1,4 @@
-FormModal = Backbone.Modal.extend(FormView);
-
-LocationModal = FormModal.extend({
+LocationModal = Backbone.Modal.extend({
   template_path: "dashboard/businesses/modals/templates/location",
   FIELDS: [
     {
@@ -33,5 +31,51 @@ LocationModal = FormModal.extend({
       "attribute": "address.zip",
       "label": "Zip Code: "
     }
-  ]
+  ],
+  events: {
+    "submit form": "save",
+    "click .save": "save",
+    "click .cancel": "cancel"
+  }, 
+  context: function(modelJson) {
+    _.each(this.FIELDS, function(field, index) {
+      field.idx = field.attribute.toString() + "-" +  index.toString();
+      field.value = _.get(modelJson, field.attribute, "");
+    });
+
+    return {
+      model: modelJson,
+      fields: this.FIELDS
+    };
+  },
+  save: function(e) {
+    var that = this;
+    e.preventDefault();
+    e.stopPropagation();
+
+    var data = Backbone.Syphon.serialize(this);
+
+    var bh = this.$el.find(".save").buttonHelper("Saving", "Saved", "Failed");
+
+    bh.loading();
+
+    this.model.save(data, {
+      success: function() {
+        bh.success();
+
+        if(that.onSave) {
+          that.onSave();
+        }
+      },
+      error: function(error) {
+        bh.failed();
+        this.$el.find(".save").tooltipHelper(error);
+      }
+    });
+  },
+  cancel: function(e) {
+    e.preventDefault();
+
+    Backbone.history.navigate("/dashboard/businesses", {trigger: true});
+  }
 });
